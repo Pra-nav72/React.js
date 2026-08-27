a16a864f86a8f4ec63d870cea10171d375f11c02
Pranav Kumar <Gyannik7772@gmail.com>
2026-08-28 03:59:03 +0530

    optimized /w help: check prev v/s latest

--- react9-project/src/App.jsx
+++ react9-project/src/App.jsx
@@ -1,60 +1,26 @@
 
-import { useState } from 'react'
+import { use, useState } from 'react'
 import './App.css'
 import PopUp from './assets/components/PopUp'
 
 function App() {
-  const [update, setUpdate] = useState('');
-  const [x, setX] = useState(false);
-  let Content = ''
-
-  if (x===true) {
-    if (update==='delete') {
-      Content = <PopUp task={update} func={handleExit}/>
-    }
-    else if (update === 'edit') {
-      Content = <PopUp task={update} func={handleExit}/>
-    }
-  }
+  const [deletePopup, setDeletePopup] = useState(false);
+  const [editPopup, setEditPopup] = useState(false);
 
 
-  function handleExit() {
-    const btnDelete = document.getElementById('btn-D')
-    const btnEdit= document.getElementById('btn-E')
-    btnDelete.hidden = false
-    btnEdit.hidden = false
-    setUpdate('')
-  }
-  function handleDelete() {
-      const btnDelete = document.getElementById('btn-D')
-      const btnEdit= document.getElementById('btn-E')
-    setUpdate('delete'); 
-    setX(true);
-    btnDelete.hidden = true;
-    btnEdit.hidden = true
-  }
-
-  function handleEdit() {
-    const btnDelete = document.getElementById('btn-D')
-    const btnEdit= document.getElementById('btn-E')
-    setUpdate('edit'); 
-    setX(true);
-    btnDelete.hidden = true;
-    btnEdit.hidden = true
-  }
   return (
 
    <div >
-      {
-        
-        <div className="min-h-screen flex items-center justify-center">
-          <button id='btn-D' onClick={handleDelete}
-            className='bg-gray-50 px-6 py-2 m-3'>Delete</button>
-          <button id='btn-E' onClick={handleEdit}
-            className='bg-gray-50 px-6 py-2 m-3'>Edit</button>
-            {Content}
-        </div>
-      }
+    <div className="min-h-screen flex items-center justify-center">
+
+        <button onClick={()=>{setDeletePopup(true)}}
+          className='bg-gray-50 px-6 py-2 m-3'>Delete</button>
+        {deletePopup && <PopUp popup={deletePopup} setPopup={setDeletePopup} title="delete" />}
+
+        <button id='btn-E' onClick={()=>{setEditPopup(true)}}
+          className='bg-gray-50 px-6 py-2 m-3'>Edit</button>
+        {editPopup && <PopUp popup={editPopup} setPopup={setEditPopup} title="edit" />}
+    </div>
       
    </div>
   )
--- react9-project/src/assets/components/PopUp.jsx
+++ react9-project/src/assets/components/PopUp.jsx
@@ -1,31 +1,18 @@
 import React from 'react'
 
-const PopUp = ({ task, func }) => {
+const PopUp = ({ popup, setPopup, title }) => {
   return (
-    <div className="relative">
-      <button
-        onClick={func}
-         className="fixed top-8 right-8 text-xl bg-gray-700 p-4 rounded-xl"
-      >
-        ❌
-      </button>
-
-      <main className="bg-gray-700 rounded-3xl flex flex-col gap-3 justify-center text-center text-white w-120 h-80">
-        <h1 className="text-2xl">⚠️ {task.toUpperCase()}</h1>
-
-        <p>Do you really want to {task} the data?</p>
-
-        <div className="mx-auto">
-          <button className="bg-green-500 py-2 px-6 mx-6">
-            Yes
-          </button>
-
-          <button className="bg-red-600 py-2 px-6" onClick={func}>
-            No
-          </button>
+    <>
+        <button className='absolute top-3 right-3 bg-gray-700 p-4 rounded-xl' onClick={()=>{setPopup(false)}}>❌</button>
+        <div className='absolute w-full h-full bg-gray-700 max-w-140 max-h-80 flex flex-col gap-8 rounded-2xl justify-center items-center text-white'>
+            <h1 className='text-3xl'>⚠️ {title.toUpperCase()}</h1>
+            <p>are you sure want to {title}?</p>
+            <div className='flex gap-6'>
+                <button className='bg-red-600 px-6 py-2 rounded-md'>Yes</button>
+                <button className='bg-green-600 px-6 py-2 rounded-md' onClick={()=>{setPopup(false)}}>No</button>
+            </div>
         </div>
-      </main>
-    </div>
+    </>
   )
 }
 
